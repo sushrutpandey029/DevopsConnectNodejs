5d4a0d5abf8249b5ece6189f296cf73a4119ea59
Sushrut Pandey <102680316+sushrutpandey029@users.noreply.github.com>
2024-10-21 16:51:04 +0530

    Devops snapshot45

--- Router/Router.js
+++ Router/Router.js
@@ -5,7 +5,7 @@ import {adduser, getuser} from '../Controller/UserController.js'
 export const router = express.Router()
 
 router.get('/', (req, res) => {
-    res.send('Hello sushrut how are you! where is you soulmate')
+    res.send('Hello sushrut how are you!')
 })
 
 router.post('/adduser',adduser);
